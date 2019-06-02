b45ae9183d3ab7fcd3bf108100bad027b35322ad
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-06-02 20:49:43 +0200

    Start creating scripts

---
+++ scripts/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir /DATA
+mkdir /DATA/docker/
+mkdir /DATA/docker/cloud
+mkdir /DATA/docker/cloud/collabora_nginx_conf
+mkdir /DATA/docker/cloud/cloud_data
+mkdir /DATA/docker/cloud/cloud_db_data
+
+mv collabora_nginx_conf/* /DATA/docker/cloud/collabora_nginx_conf
