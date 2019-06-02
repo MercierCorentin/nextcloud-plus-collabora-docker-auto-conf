c8f9b23006c84c0ea60d239e36eb78a46ada9382
MercierCorentin <corentin.mercier.pro@gmail.com>
2019-06-02 21:08:27 +0200

    Fixing bugs on init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,4 +9,4 @@ mkdir /DATA/docker/cloud/collabora_nginx_conf
 mkdir /DATA/docker/cloud/cloud_data
 mkdir /DATA/docker/cloud/cloud_db_data
 
-mv collabora_nginx_conf/* /DATA/docker/cloud/collabora_nginx_conf
+mv ../collabora_nginx_conf/* /DATA/docker/cloud/collabora_nginx_conf
