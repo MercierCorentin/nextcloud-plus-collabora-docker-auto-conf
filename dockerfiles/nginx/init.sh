323e7a0a80a5890bd99b043a7ef402dfc65f38fd
Corentin Mercier <corentin.mercier@etu.utc.fr>
2019-06-04 12:48:01 +0200

    Change docker-compose.yml according to env file and fixing bug in init nginx script

--- dockerfiles/nginx/init.sh
+++ dockerfiles/nginx/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i -e "s/domain_name/$1/g" /etc/nginx/site-available/default
+sed -i -e "s/domain_name/$1/g" /etc/nginx/sites-available/default
 sed -i -e "s/domain_name/$1/g" /etc/nginx/nginx.conf
 
